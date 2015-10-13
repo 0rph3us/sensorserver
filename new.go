276ae4becea0c4ff93d88915cd48c93f3d40d5ea
Michael Rennecke <michael.rennecke@gmail.com>
2015-10-13 16:39:41 +0200

    clenup code and remove unused stuff

--- new.go
+++ new.go
@@ -1,16 +1,15 @@
 package sensorserver
 
 import (
-    "github.com/boltdb/bolt"
-    "log"
+	"github.com/boltdb/bolt"
+	"log"
 )
 
 func New(filename string) (s *Sensorserver, err error) {
-    s = &Sensorserver{}
-    s.boltdb, err = bolt.Open(filename, 0600, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-//    defer s.boltdb.Close()
-    return
+	s = &Sensorserver{}
+	s.boltdb, err = bolt.Open(filename, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
 }
