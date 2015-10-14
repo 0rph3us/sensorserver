0cef158372ea6b709a4a8d9c3ed29ff791d5cf29
Michael Rennecke <michael.rennecke@gmail.com>
2015-10-14 21:52:57 +0200

    move type Sensordata into file types.go

--- types.go
+++ types.go
@@ -1,5 +1,9 @@
 package sensorserver
 
+import (
+	"github.com/boltdb/bolt"
+)
+
 type putdata struct {
 	Timestamp int
 	Value     float32
@@ -8,4 +12,9 @@ type putdata struct {
 type Config struct {
 	Database string `toml:"database"`
 	Title    string `toml:"title"`
+}
+
+type Sensorserver struct {
+	boltdb *bolt.DB
+	conf   Config
 }
