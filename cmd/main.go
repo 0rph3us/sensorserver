e1eee426c18a2a770adef6fed03057856a7e2d22
Michael Rennecke <michael.rennecke@gmail.com>
2015-10-14 07:50:32 +0200

    format cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	".."
-    "log"
+	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
 
-    s, err := sensorserver.New("my_4.db")
-    if err != nil {
-        log.Fatal(err)
-    }
+	s, err := sensorserver.New("my_4.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	router := gin.Default()
 
 	router.GET("/boltdb/backup", s.Backup)
 	router.HEAD("/boltdb/backup", s.Backup)
 
-    router.GET("/boltdb/stats", s.Stats)
-    router.HEAD("/boltdb/stats", s.Stats)
+	router.GET("/boltdb/stats", s.Stats)
+	router.HEAD("/boltdb/stats", s.Stats)
 
 	router.GET("/sensor", s.GetSensors)
-    router.PUT("/sensor/:name", s.PutSensorData)
+	router.PUT("/sensor/:name", s.PutSensorData)
 	router.GET("/sensor/:name", s.GetSensorData)
-
 
 	router.Run(":8080")
 
