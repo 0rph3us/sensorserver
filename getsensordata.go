25641b3b7c2e1f08ad76e747c947c0f774556caa
Michael Rennecke <michael.rennecke@gmail.com>
2015-10-14 07:07:42 +0200

    don't crash, if the bucket don't exist -> return error

--- getsensordata.go
+++ getsensordata.go
@@ -1,6 +1,7 @@
 package sensorserver
 
 import (
+	"errors"
 	"github.com/boltdb/bolt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -9,20 +10,23 @@ import (
 func (s *Sensorserver) GetSensorData(c *gin.Context) {
 	var data []putdata
 
-	sensor := []byte(c.Param("name"))
+	sensor := c.Param("name")
 
 	// return all Values, without parameter
 	err := s.boltdb.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(sensor)
+		b := tx.Bucket([]byte(sensor))
+		if b == nil {
+			msg := "Can't get Data for " + sensor
+			return errors.New(msg)
+		}
 		b.ForEach(func(k, v []byte) error {
 			data = append(data, putdata{BytesToInt(k), BytesToFloat32(v)})
 			return nil
 		})
 		return nil
 	})
-
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	} else {
 		c.JSON(http.StatusOK, data)
 	}
