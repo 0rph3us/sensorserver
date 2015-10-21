928953d5163755fe119edb8dcdc167938729c252
Michael Rennecke <michael.rennecke@gmail.com>
2015-10-21 21:21:05 +0200

    add helper script for convert mysql to json

---
+++ helper/createjson.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+import _ "github.com/go-sql-driver/mysql"
+
+type data struct {
+	Timestamp int
+	Value     float32
+}
+
+func writeFile(d []data, name string) {
+	var j []byte
+
+	j, err := json.Marshal(d)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if err := ioutil.WriteFile(name, j, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	var insert_time int
+	var temp_bmp085, tmp_dth22, pressure, p_sea, humidity float32
+
+	user := "foo"
+	pass := "bar"
+	prot := "tcp"
+	addr := "localhost:3306"
+	dbname := "sensoren"
+	netAddr := fmt.Sprintf("%s(%s)", prot, addr)
+	dsn := fmt.Sprintf("%s:%s@%s/%s?timeout=30s&strict=true&parseTime=true", user, pass, netAddr, dbname)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	// Prepare statement for reading data
+	stmt, err := db.Prepare("SELECT UNIX_TIMESTAMP(insert_time), temp_bmp085, tmp_dth22, pressure, p_sea, humidity FROM test WHERE insert_time > ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+
+	rows, err := stmt.Query("2012-10-10 22:30:05")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var j_temp_bmp085, j_tmp_dth22, j_pressure, j_p_sea, j_humidity []data
+	for rows.Next() {
+		err := rows.Scan(&insert_time, &temp_bmp085, &tmp_dth22, &pressure, &p_sea, &humidity)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		j_temp_bmp085 = append(j_temp_bmp085, data{insert_time, temp_bmp085})
+		j_tmp_dth22 = append(j_tmp_dth22, data{insert_time, tmp_dth22})
+		j_pressure = append(j_pressure, data{insert_time, pressure})
+		j_p_sea = append(j_p_sea, data{insert_time, p_sea})
+		j_humidity = append(j_humidity, data{insert_time, humidity})
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	writeFile(j_temp_bmp085, "temp_bmp085")
+	writeFile(j_tmp_dth22, "tmp_dth22")
+	writeFile(j_pressure, "pressure")
+	writeFile(j_p_sea, "p_sea")
+	writeFile(j_humidity, "humidity")
+}
