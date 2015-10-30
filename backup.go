39a541a103515aea4775c271531852138ddf6917
Michael Rennecke <michael.rennecke@gmail.com>
2015-10-30 07:47:14 +0100

    backup filename contains the date

--- backup.go
+++ backup.go
@@ -5,12 +5,18 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
+	"time"
 )
 
+// return the hole database file
 func (s *Sensorserver) Backup(c *gin.Context) {
 
+	// filename contains the date
+	time := time.Now()
+	content_disposition := `attachment; filename="` + time.Format("2006-01-02_15:04") + `-sensorserver.db"`
+
 	err := s.boltdb.View(func(tx *bolt.Tx) error {
-		c.Header("Content-Disposition", `attachment; filename="my.db"`)
+		c.Header("Content-Disposition", content_disposition)
 		c.Header("Content-Length", strconv.Itoa(int(tx.Size())))
 		_, err := tx.WriteTo(c.Writer)
 
