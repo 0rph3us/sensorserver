ad861b2893155385b7f6eef9e69ae6d8984b0362
Michael Rennecke <michael.rennecke@gmail.com>
2015-11-02 18:35:38 +0100

    add 3 month min-max link

--- page.go
+++ page.go
@@ -10,5 +10,10 @@ func (s *Sensorserver) Page(c *gin.Context) {
 	// configure the sub title
 	_ = s.duration(c.Query("duration"))
 
+	s.conf.Type = c.Query("type")
+	if s.conf.Type == "" {
+		s.conf.Type = "single"
+	}
+
 	c.HTML(http.StatusOK, "index.html", s.conf)
 }
